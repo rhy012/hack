f449516af73f47b4c00f37fc8d6f291665c7ffea
rhy012 <129658530+rhy012@users.noreply.github.com>
2023-04-23 12:35:16 +0530

    4th commit

--- app.py
+++ app.py
@@ -1,27 +1,30 @@
-from flask import Flask, request, render_template, jsonify
-import openai
+from flask import Flask, request, render_template
+import openai 
 import os
 
 app = Flask(__name__)
 
 # Set up OpenAI API key
-openai.api_key = 'API_KEY'
+openai.api_key = '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' 
 
 @app.route('/')
-def index():
+def chat():
     return render_template('chat.html')
 
 @app.route('/api', methods=['POST'])
-def get_response():
-    message = request.form['message']
+def api():
+    message = request.json.get("message")
     
     completion = openai.ChatCompletion.create(
       model="gpt-3.5-turbo",
       message=[
         {"role":"user","content": message}
       ]
-    )
-    return completion.choices[0].message
+    ) 
+    if completion.choices[0].message!=None:
+      return completion.choices[0].message 
+    else:
+      return 'Failed to Generate response!'
 
 if __name__ == '__main__':
     app.run(debug=True)
